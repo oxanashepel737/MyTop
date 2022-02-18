bafa7cc8d73bd4593ce54d5ab9fcb6bc55061600
oxana737 <oxanashepel737@gmail.com>
2022-02-18 17:30:17 +0200

    p, tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Htag} from "../components";
+import {Button, Htag, P, Tag} from "../components";
 
 export default function Home(): JSX.Element {
     return (
@@ -7,6 +7,13 @@ export default function Home(): JSX.Element {
             <Htag tag='h1'>Тект</Htag>
             <Button appearance="primary" arrow='right'>Кнопка</Button>
             <Button appearance="ghost" arrow='down'>Кнопка</Button>
+            <P size='l'>Большой</P>
+            <P>Средний</P>
+            <P size='s'>Маленький</P>
+            <Tag size='s'>Ghost</Tag>
+            <Tag size='m' color='red'>Red</Tag>
+            <Tag size='m' color='green'>Green</Tag>
+            <Tag color='primary'>Primary</Tag>
         </>
     )
 }
