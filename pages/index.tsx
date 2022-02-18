10d709ab55d76c12b76b750c4329740dc59c109e
oxana737 <oxanashepel737@gmail.com>
2022-02-18 15:52:41 +0200

    button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {Htag} from "../components";
+import {Button, Htag} from "../components";
 
 export default function Home(): JSX.Element {
     return (
         <>
             <Htag tag='h1'>Тект</Htag>
+            <Button appearance="primary" arrow='right'>Кнопка</Button>
+            <Button appearance="ghost" arrow='down'>Кнопка</Button>
         </>
     )
 }
