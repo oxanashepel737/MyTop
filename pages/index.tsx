739cbeace81d671295b486fd6badb0c475305f5d
oxana737 <oxanashepel737@gmail.com>
2022-02-18 11:36:36 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Htag} from "../components";
+
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <Htag tag='h1'>Тект</Htag>
+        </>
+    )
+}
